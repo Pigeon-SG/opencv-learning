be5cee1b47e1b5c39dd11262dab7826781119b55
shangguan <Pigeon-SG@163.com>
2021-01-08 20:59:07 +0800

    封装表情包库完成~

--- Pigeon_utils.py
+++ Pigeon_utils.py
@@ -1,15 +1,69 @@
 import cv2
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import paddlehub as hub
-import os
 
+from PIL import Image, ImageDraw, ImageFont
+import os
+def emoji_addtext(emoji,text):
+    # cv2 读取处理过的emoji图片
+    Pilimg = Image.fromarray(emoji)
+    draw = ImageDraw.Draw(Pilimg)
+    font = ImageFont.truetype('simhei.ttf',60)
+    # 文字居中处理
+    fsize = font.getsize(text)
+    #       Y轴居中
+    text_anchor_y = emoji.shape[0] - 50 - fsize[1]//2
+    #       X轴向居中
+    text_anchor_x = (emoji.shape[1] - fsize[0])//2
+    draw.text((text_anchor_x,text_anchor_y),text,font=font,fill=0)
+    emoji_text = cv2.cvtColor(np.array(Pilimg),cv2.COLOR_RGB2BGR)
+    return emoji_text
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
 def plt_show(img):
     imageRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     plt.imshow(imageRGB)
     plt.show()
+
+def pic_resizebytimes(img,times):
+    ret = cv2.resize(img,(int(img.shape[1] * times),int(img.shape[0] * times)))
+    return ret
+
+
 def pic_insert(back_img,object_img,center_point,scale = 1):
-    # center 就按照 cv 存贮的标准来
+    #根据输入object_img的大小，插入背景back_img中
+    #center_point 用于计算插入坐标
+    #scale 缩小倍数
     back_insert_object = back_img
     object_resize = cv2.resize(object_img,(int(object_img.shape[1]/scale),int(object_img.shape[0]/scale)))
     # 获取替代框坐标
@@ -22,8 +76,9 @@ def pic_insert(back_img,object_img,center_point,scale = 1):
     return back_insert_object
 
 def pic_fetch(back_img,object_img,center_point,scale = 1):
-    # 根据输入的 目标图像大小，从背景中抠出相应大小的图像
-    # center 就按照 cv 存贮的标准来
+    # 根据输入object_img目标图像大小，从背景back_img中抠出相应大小的图像
+    # center_point 用于计算抠图的坐标位置
+    # scale 缩小倍数
     back_fetch_object = back_img
     object_resize = cv2.resize(object_img, (int(object_img.shape[1] / scale), int(object_img.shape[0] / scale)))
     # 获取替代框坐标
@@ -36,14 +91,45 @@ def pic_fetch(back_img,object_img,center_point,scale = 1):
     result = back_img[height_min:height_max,width_min:width_max]
     return result
 
+matplotlib.use("TkAgg")
+
 def get_contour_outlines(img):
     human_seg = hub.Module(name="humanseg_mobile")
-    pass
+    res = human_seg.segment(images=[img], visualization=False)
+    img_seg = res[0]["data"]
+    # 获取正反面膜
+    ret, face_contour = cv2.threshold(img_seg, 100, 255, cv2.THRESH_BINARY)
+    ret, face_contour_inv = cv2.threshold(img_seg, 100, 255, cv2.THRESH_BINARY_INV)
+    return face_contour,face_contour_inv
+
+def embedding_face(box_fromback,face_img,mask,mask_inv):
+    # 取出合适大小的嵌入框作为嵌入的小目标
+    back = cv2.bitwise_and(box_fromback,box_fromback,mask = mask_inv)
+    face = cv2.bitwise_and(face_img,face_img,mask=mask)
+    box_processed = cv2.add(back,face)
+    return box_processed
+
+def add_textmargin(img,text):
+    padding = np.zeros_like(img)
+    padding[:] = 255
+    padding = cv2.resize(padding, (padding.shape[1], 100))
+    ret = np.vstack([img, padding])
+    emoji = emoji_addtext(ret,text)
+    return emoji
 
 if __name__ == "__main__":
-    back = np.zeros((500,500),np.uint8)
-    pic = np.zeros((50,50),np.uint8)
-    pic[:] = 255
-    ret = pic_insert(back,pic,(200,200),scale=1.5)
-    cv2.imshow("1",ret)
-    cv2.waitKey(0)
+    img = cv2.imread("pics/wyh.jpg")
+    img = pic_resizebytimes(img,1/6.0)
+    mask,mask_inv = get_contour_outlines(img)
+    ret = stackImages(1,[[img,mask_inv,mask]])
+    #取出合适大小的框
+    back = cv2.imread("pics/pigeon.jpg")
+    center_pts = (70, 169)
+    box_fromback = pic_fetch(back,img,center_point=center_pts,scale=1.0)
+    #制作背景和头像，进行相加
+    ret = embedding_face(box_fromback,img,mask,mask_inv)
+    ret = pic_insert(back,ret,center_pts,1)
+    ret = add_textmargin(ret,"今晚啥时候？")
+    ret = cv2.cvtColor(ret,cv2.COLOR_BGR2RGB)
+    plt_show(ret)
+
--- 王雨涵表情包.py
+++ 王雨涵表情包.py
@@ -1,49 +1,15 @@
-import cv2
-import imagestack
-import matplotlib.pyplot as plt
-import numpy as np
-import Pigeon_utils as pigeon
-def plt_show(img):
-    imageRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    plt.imshow(imageRGB)
-    plt.show()
-face_contour = cv2.imread("pics_processed/wyh.jpg")
-face_contour = cv2.cvtColor(face_contour,cv2.COLOR_RGB2GRAY)
-#face_contour_binary 得到了汪雨涵的头像轮廓
-ret,face_contour_binary = cv2.threshold(face_contour,100,255,cv2.THRESH_BINARY)
-ret,face_contour_inv = cv2.threshold(face_contour,100,255,cv2.THRESH_BINARY_INV)
-
-
-#resize 至符合嵌入的大小
-face_contour_resize = cv2.resize(face_contour_binary,(face_contour_binary.shape[1]//6,face_contour_binary.shape[0]//6))
-face_contour_inv_resize = cv2.resize(face_contour_inv,(face_contour_binary.shape[1]//6,face_contour_binary.shape[0]//6))
-
-face = cv2.imread("pics/wyh.jpg")
-
-face_withoutback = cv2.bitwise_and(face,face,mask=face_contour_binary)
-face_withoutback = cv2.resize(face_withoutback,(face_withoutback.shape[1]//6,face_withoutback.shape[0]//6))
-
-plt_show(face_withoutback)
-
-# 背景加载
+from Pigeon_utils import *
+img = cv2.imread("pics/wyh.jpg")
+img = pic_resizebytimes(img,1/6.0)
+mask,mask_inv = get_contour_outlines(img)
+ret = stackImages(1,[[img,mask_inv,mask]])
+#取出合适大小的框
 back = cv2.imread("pics/pigeon.jpg")
-#制造 back_mask
-back_mask = np.zeros_like(cv2.cvtColor(back,cv2.COLOR_BGR2GRAY))
-center_pts = (70,169)
-back_mask = pigeon.pic_insert(back_mask,face_contour_resize,center_pts)
-
-face_fromback = pigeon.pic_fetch(back,face_contour_resize,center_pts)
-face_fromback = cv2.bitwise_and(face_fromback,face_fromback,mask=face_contour_inv_resize)
-
-emoji_wyh_face = cv2.add(face_withoutback,face_fromback)
-
-emoji_wyh = pigeon.pic_insert(back,emoji_wyh_face,center_pts)
-# 加入空白
-
-padding = np.zeros_like(emoji_wyh)
-padding[:] = 255
-padding = cv2.resize(padding,(padding.shape[1],100))
-ret = np.vstack([emoji_wyh,padding])
+center_pts = (70, 169)
+box_fromback = pic_fetch(back,img,center_point=center_pts,scale=1.0)
+#制作背景和头像，进行相加
+ret = embedding_face(box_fromback,img,mask,mask_inv)
+ret = pic_insert(back,ret,center_pts,1)
+ret = add_textmargin(ret,"今晚啥时候？")
+ret = cv2.cvtColor(ret,cv2.COLOR_BGR2RGB)
 plt_show(ret)
-
-cv2.imwrite("pics_processed/wyh_emoji.jpg",ret)
